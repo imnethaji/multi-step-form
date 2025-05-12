25fd9ce1f81c28b0a7e052863cc7a1676b23164a
Nethaji <reachnethajip@gmail.com>
2025-05-12 22:35:57 +0530

    logic update

--- src/js.js
+++ src/js.js
@@ -1,35 +1,69 @@
 let currentStep = 1;
-
-//  Toggle Monthly and Annual prices
-const prices = {
-  monthly: { arcade: 9, advanced: 12, pro: 15 },
-  annually: { arcade: 90, advanced: 120, pro: 150 },
+let formData = {
+  currentStep: 1,
+  name: null,
+  email: null,
+  phoneNumber: null,
+  planName: null,
+  planTenure: "monthly",
+  planAddOns: [],
+  total: 0,
 };
 
-let pricing = "monthly";
+localStorage.setItem("localFormData", JSON.stringify(formData));
+
+//  Toggle between Monthly and Annual prices
+const prices = {
+  plans: { arcade: 9, advanced: 12, pro: 15 },
+  addOnPrices: {
+    onlineService: { title: "Online Service", price: 1 },
+    largerStorage: { title: "Larger Storage", price: 2 },
+    customProfile: { title: "Customizable Profile", price: 2 },
+  },
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("price-toggle");
   const toggleDiv = document.querySelector(".toggle");
 
   function toggle() {
-    let arcadePrice = document.getElementById("arcadePlanPrice");
-    let advancedPrice = document.getElementById("advancedPlanPrice");
-    let proPrice = document.getElementById("proPlanPrice");
+    const arcadePrice = document.getElementById("arcadePlanPrice");
+    const advancedPrice = document.getElementById("advancedPlanPrice");
+    const proPrice = document.getElementById("proPlanPrice");
+    const onlineServicePrice = document.getElementById("onlineServicePrice");
+    const largerStoragePrice = document.getElementById("largerStoragePrice");
+    const customProfilePrice = document.getElementById("customProfilePrice");
     toggleDiv.classList.toggle("right");
 
-    if (pricing === "monthly") {
-      arcadePrice.innerHTML = prices.annually.arcade;
-      advancedPrice.innerHTML = prices.annually.advanced;
-      proPrice.innerHTML = prices.annually.pro;
-      pricing = "annually";
+    if (formData.planTenure === "monthly") {
+      const term = "yr";
+      // Switch to annual prices
+      arcadePrice.innerHTML = `$${prices.plans.arcade * 10}/+${term}`;
+      advancedPrice.innerHTML = `$${prices.plans.advanced * 10}/+${term}`;
+      proPrice.innerHTML = `$${prices.plans.pro * 10}/+${term}`;
+      onlineServicePrice.innerHTML = `$${
+        prices.addOnPrices.onlineService.price * 10
+      }/+${term}`;
+      largerStoragePrice.innerHTML = `$${
+        prices.addOnPrices.largerStorage.price * 10
+      }/+${term}`;
+      customProfilePrice.innerHTML = `$${
+        prices.addOnPrices.customProfile.price * 10
+      }/+${term}`;
+      formData.planTenure = "annually";
     } else {
-      arcadePrice.innerHTML = prices.monthly.arcade;
-      advancedPrice.innerHTML = prices.monthly.advanced;
-      proPrice.innerHTML = prices.monthly.pro;
-      pricing = "monthly";
+      const term = "mo";
+      // Switch to monthly prices
+      arcadePrice.innerHTML = `$${prices.plans.arcade}/+${term}`;
+      advancedPrice.innerHTML = `$${prices.plans.advanced}/+${term}`;
+      proPrice.innerHTML = `$${prices.plans.pro}/+${term}`;
+      onlineServicePrice.innerHTML = `$${prices.addOnPrices.onlineService.price}/+${term}`;
+      largerStoragePrice.innerHTML = `$${prices.addOnPrices.largerStorage.price}/+${term}`;
+      customProfilePrice.innerHTML = `$${prices.addOnPrices.customProfile.price}/+${term}`;
+      formData.planTenure = "monthly";
     }
   }
+
   toggleButton.addEventListener("click", toggle);
 });
 
@@ -41,6 +75,8 @@ function nextStep() {
     currentStepElement.classList.remove("active");
     nextStepElement.classList.add("active");
     currentStep++;
+    formData.currentStep = currentStep;
+    localStorage.setItem("localFormData", JSON.stringify(formData));
   }
 }
 
@@ -116,11 +152,14 @@ function confirmSubscription() {
 
 document.addEventListener("DOMContentLoaded", function () {
   const planInfoElements = document.querySelectorAll(".plan-info");
+  let totalPriceDisplay = document.getElementById("final-plan-price");
+  let planPrice = "90/yr";
+  totalPriceDisplay.innerHTML = planPrice;
 
   planInfoElements.forEach(function (planInfoElement) {
     planInfoElement.addEventListener("click", function () {
       // Remove 'plan-selected' class from all plan info elements
-      planInfoElements.forEach(function (element) {
+      planInfoElements.forEach((element) => {
         element.classList.remove("plan-selected");
       });
 
@@ -128,17 +167,33 @@ document.addEventListener("DOMContentLoaded", function () {
       this.classList.add("plan-selected");
 
       // Add your logic for handling the click event here
-      console.log("working");
+      let planName = planInfoElement
+        .querySelector("h4")
+        .textContent.toLowerCase();
+      formData.planName = planName;
+      planPrice = prices.plans[planName];
+      totalPriceDisplay.innerHTML = planPrice;
+      console.log(planName);
     });
   });
 });
 
 //Checkbox toggling logic
 
-function toggleCheckbox(checkboxId) {
+function addonToggleCheckbox(checkboxId) {
   const checkbox = document.getElementById(checkboxId);
-
-  if (checkbox) {
-    checkbox.checked = !checkbox.checked;
+  if (!checkbox) return;
+  checkbox.checked = !checkbox.checked;
+  const index = formData.planAddOns.indexOf(checkboxId);
+  if (checkbox.checked && index === -1) {
+    formData.planAddOns.push(checkboxId);
+  } else if (!checkbox.checked && index !== -1) {
+    formData.planAddOns.splice(index, 1);
   }
+
+  console.log("Current Add-ons:", formData.planAddOns);
+}
+
+function tenureChange() {
+  formData.planTenure = "monthly" ? "monthly" : "yearly";
 }
